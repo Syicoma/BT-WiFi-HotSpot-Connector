a219cac7458eb7574e8906c2d2301bbd172b91fc
Syicoma <srycroft@gmail.com>
2021-03-14 15:05:17 +0000

    Update bt_conn.sh

--- bt_conn.sh
+++ bt_conn.sh
@@ -8,10 +8,26 @@
 
 # Config
 printf "BT-WiFi Keep Alive v1.0\nCreated by: Syicoma\n\n"
-USERNAME=<login_email>
-PASSWORD=<password>
+USERNAME=YOUR_LOGIN_EMAIL
+PASSWORD=YOUR_PASSWORD
 URL=https://www.btwifi.com:8443/home
 CONN_URL=https://www.btwifi.com:8443/ante?partnerNetwork=btb
+
+# Send notify to all users
+SEND_ALL () {
+PATH=/usr/bin:/bin
+
+XUSERS=($(who|grep -E "\(:[0-9](\.[0-9])*\)"|awk '{print $1$NF}'|sort -u))
+for XUSER in "${XUSERS[@]}"; do
+        NAME=(${XUSER/(/ })
+        DISPLAY=${NAME[1]/)/}
+        DBUS_ADDRESS=unix:path=/run/user/$(id -u ${NAME[0]})/bus
+        sudo -u ${NAME[0]} DISPLAY=${DISPLAY} \
+                DBUS_SESSION_BUS_ADDRESS=${DBUS_ADDRESS} \
+                PATH=${PATH} \
+                notify-send "$@"
+done
+}
 
 # Check log file exists
 if ! [ -f bt_conn.log ]; then
@@ -21,11 +37,16 @@ else
 fi
 
 while true; do
-	IS_ONLINE=$(wget $URL -O - 2>/dev/null | grep -o accountLogoff)	
+	IS_ONLINE=$(wget $URL -q -O - 2>/dev/null | grep -o accountLogoff)	
 	if [ "$IS_ONLINE" ]; then
 		sleep 10
 	else
 		IS_ONLINE=false
+		#### Desktop Notification
+		SEND_ALL "
+		BT-WiFi Connection Dropped
+		Reconnecting @ $(date)
+		"
 		echo "Reconnecting BT-WiFi...." & disown
 		curl -d "username=$USERNAME&password=$PASSWORD" $CONN_URL
 		echo "Reconnected @ $(date)" >> bt_conn.log
